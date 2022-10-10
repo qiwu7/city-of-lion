34e402926487af87471a8c676c2482d133f695fe
Qi Wu <qiwu@alumni.stanford.edu>
2022-10-10 17:50:34 +0800

    ERC721Metadata

--- src/contracts/KryptoBirdz.sol
+++ src/contracts/KryptoBirdz.sol
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract KryptoBird {
-    string public name;
-    string public symbol;
+import './ERC721Connector.sol';
 
-    constructor() {
-        name = 'KryptoBird';
-        symbol = 'KBIRDZ';
+contract KryptoBird is ERC721Connector {
+
+    constructor() ERC721Connector('KryptoBird', 'KBIRDZ') {
     }
 }
